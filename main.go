3ddf0995a0c8882bbd3d493e7ac108336da57acc
Rohan <rohanam2000@gmail.com>
2026-02-15 18:50:07 +0530

    Simple fix push

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello world");
+func Hello() {
+	fmt.Println("Hello world")
 }
---
+++ simple1_server.go
@@ -0,0 +1,20 @@
+// package main
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	
+	
+)
+
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello, I love you %s!", r.URL.Path[1:])
+}
+
+func main(){
+	http.HandleFunc("/", handler);
+	fmt.Println("Server running on port: 8080")
+	log.Fatal(http.ListenAndServe(":8080",nil));
+}
