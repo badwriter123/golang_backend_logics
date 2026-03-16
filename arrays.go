5f715a09a60b1ab1c7e19955d146b478d934b41b
Rohan <rohanam2000@gmail.com>
2026-03-16 16:44:11 +0530

    Array iteration and function

--- arrays.go
+++ arrays.go
@@ -8,7 +8,29 @@ func arr() {
 
 	// arr := [2][2]int{{1,2}, {0,0}};
 
-	arr := [...][2]int{{1,2}, {2,2}, {3,2}};
-	arr[0] = [2]int{10, 11};
-	fmt.Println(len(arr));
+	// arr := [...][2]int{{1,2}, {2,2}, {3,2}};
+	// arr[0] = [2]int{10, 11};
+	// fmt.Println(len(arr));
+
+	// Looping through the array
+	// for value := range arr {
+	// 	fmt.Println(value)
+	// }
+
+	// Looping through nested arrays
+	// arr1 := [...][2]int{{1,2}, {2,4}, {2,2}};
+
+	// for _, nested := range arr1 {
+	// 	for _, value := range nested {
+	// 		fmt.Println(value);
+	// 	}
+	// }
+
+	arr := [...][2]int{{1,2}, {2,2}, {3,2}}
+	test(arr)
+	fmt.Println(arr);
+}
+
+func test(arr [3][2]int) {
+	arr[0] = [2]int{100, 100}
 }
